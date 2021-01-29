8a6c00b5e4333b5aac7eaa57e238b28ae31a5935
Carter Bush <carter.l.bush@gmail.com>
2021-01-29 17:03:33 +1100

    Boiler plate

---
+++ src/profile-card-component/ProfileCardComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ProfileCardComponent = () => {
+    return <div></div>
+}
+
+export default ProfileCardComponent
---
+++ src/profile-card-component/ProfileCardComponentScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProfileCardComponent from './ProfileCardComponent'
+
+const ProfileCardComponentScreen = () => {
+    return (
+        <div>
+            <ProfileCardComponent />
+        </div>
+    )
+}
+
+export default ProfileCardComponentScreen
